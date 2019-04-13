1664a870e12dceccf7e717b4e81cf5cb4eeb59b2
thule <lunthu@gmail.com>
2019-04-14 00:22:38 +0900

    completedwith order, payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,9 @@ class OrdersController < ApplicationController
     redirect_to new_order_payment_path(order)
   end
 
+  def show
+    @order = current_user.orders.where(state: 'paid').find(params[:id])
+  end
+
+
 end
